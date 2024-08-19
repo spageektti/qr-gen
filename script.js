ef547669048f2a325a349e03c28dc7252749eb55
spageektti <git@spageektti.cc>
2024-08-19 21:11:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ document.getElementById('generate-button').addEventListener('click', function ()
     var colorDark = document.getElementById('color-dark').value;
     var colorLight = document.getElementById('color-light').value;
 
+    var wifiSSID = document.getElementById('wifi-ssid').value;
+    var wifiPassword = document.getElementById('wifi-password').value;
+    var wifiEncryption = document.getElementById('wifi-encryption').value;
+
+    if (wifiSSID) {
+        qrContent = `WIFI:S:${wifiSSID};T:${wifiEncryption};P:${wifiPassword};;`;
+    }
+
     var qrCodeElement = document.getElementById('qr-code');
     qrCodeElement.innerHTML = "";
 
