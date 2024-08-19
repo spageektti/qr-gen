937cdd3a3d0fe66419c8f0f5cf3b39a6b3748ae5
spageektti <git@spageektti.cc>
2024-08-19 21:11:00 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 document.getElementById('generate-button').addEventListener('click', function () {
     var qrContent = document.getElementById('qr-content').value;
+    var qrWidth = parseInt(document.getElementById('qr-width').value);
+    var qrHeight = parseInt(document.getElementById('qr-height').value);
+    var colorDark = document.getElementById('color-dark').value;
+    var colorLight = document.getElementById('color-light').value;
 
     var qrCodeElement = document.getElementById('qr-code');
     qrCodeElement.innerHTML = "";
@@ -7,6 +11,10 @@ document.getElementById('generate-button').addEventListener('click', function ()
     if (qrContent.trim()) {
         new QRCode(qrCodeElement, {
             text: qrContent,
+            width: qrWidth,
+            height: qrHeight,
+            colorDark: colorDark,
+            colorLight: colorLight,
             correctLevel: QRCode.CorrectLevel.H
         });
     }
